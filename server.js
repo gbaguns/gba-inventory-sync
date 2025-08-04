345b516dd21ee1962f51b6cade2732d5420d3d1f
gbaguns <sales@gbaguns.com>
2025-08-04 11:31:50 -0400

    Update server.js

--- server.js
+++ server.js
@@ -9,22 +9,26 @@ const app = express();
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const uploadsDir = path.join(__dirname, 'uploads');
 
+if (!fs.existsSync(uploadsDir)) fs.mkdirSync(uploadsDir);
+
 app.use(fileUpload());
-app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.send(`
+  const html = `
     <h2>Upload Inventory Files</h2>
     <form method="POST" encType="multipart/form-data">
-      <input type="file" name="files" multiple /><br /><br />
+      <input type="file" name="files" multiple><br><br>
       <button type="submit">Upload Files</button>
     </form>
-    <br />
+    <br><hr><br>
     <form method="POST" action="/run">
       <button type="submit">Run Aggregation & Update BigCommerce</button>
     </form>
-  `);
+    <br>
+    <a href="/files">View Uploaded Files</a>
+  `;
+  res.send(html);
 });
 
 app.post('/', (req, res) => {
@@ -34,17 +38,23 @@ app.post('/', (req, res) => {
 
   const uploaded = Array.isArray(req.files.files) ? req.files.files : [req.files.files];
   uploaded.forEach(file => {
-    file.mv(path.join(uploadsDir, file.name));
+    const filepath = path.join(uploadsDir, file.name);
+    file.mv(filepath);
   });
 
-  res.send('Files uploaded successfully. <a href="/">Back</a>');
+  res.send('Files uploaded successfully.<br><a href="/">Back</a>');
 });
 
 app.post('/run', (req, res) => {
   exec('node runAggregator.js', (err, stdout, stderr) => {
-    if (err) return res.send(`Error: ${stderr}`);
+    if (err) return res.send(`<pre>Error:\n${stderr}</pre><a href="/">Back</a>`);
     res.send(`<pre>${stdout}</pre><br><a href="/">Back</a>`);
   });
 });
 
-app.listen(3000, () => console.log('Web dashboard running on port 3000'));
+app.get('/files', (req, res) => {
+  const files = fs.readdirSync(uploadsDir);
+  res.send(`<ul>${files.map(f => `<li>${f}</li>`).join('')}</ul><a href="/">Back</a>`);
+});
+
+app.listen(3000, () => console.log('Inventory Dashboard running on port 3000'));
