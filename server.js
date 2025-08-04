c3edef1655c0dd9733d15eafb327db9d8f4e5d5e
gbaguns <sales@gbaguns.com>
2025-08-04 17:20:14 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import session from 'express-session';
 import path from 'path';
 import fs from 'fs';
 import { fileURLToPath } from 'url';
-import { aggregateInventory } from './runaggregator.js';
+import { aggregateInventory } from './runAggregator.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
